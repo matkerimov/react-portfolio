c7bdfce3940e583535aeaa03c5832c02aade3fab
kurkuroomatkerimov <kurkuroomatkerimov@gmail.com>
2023-01-22 02:02:13 +0600

    done

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,4 +1,7 @@
-import { useEffect, useRef} from 'react';
+import {
+    useEffect,
+    // useRef
+       } from 'react';
 import {useLocalStorage} from "../../utils/useLoscalStorege";
 import {detectDarkMode} from "../../utils/detectDarkMode";
 import sun from "./sun.svg";
@@ -8,20 +11,20 @@ import "./style.css"
 const BtnDarkMode = () => {
     const [darkMode, setDarkMode] = useLocalStorage('darkMode', detectDarkMode())
 
-    const btnRef = useRef(null)
+    // const btnRef = useRef(null)
 
     useEffect(() => {
         if (darkMode === 'dark') {
             document.body.classList.add('dark')
-            btnRef.current.classList.add('dark-mode-btn--active')
+            // btnRef.current.classList.add('dark-mode-btn--active')
         } else {
             document.body.classList.remove('dark')
-            btnRef.current.classList.remove('dark-mode-btn--active')
+            // btnRef.current.classList.remove('dark-mode-btn--active')
         }
     }, [darkMode])
 
 
-    useEffect(()=>{
+    useEffect(() => {
 
         window
             .matchMedia("(prefers-color-scheme: dark)")
@@ -29,7 +32,7 @@ const BtnDarkMode = () => {
                 const newColorScheme = event.matches ? "dark" : "light";
                 setDarkMode(newColorScheme)
             });
-    }, [])
+    }, [setDarkMode])
 
 
     const toggleDarkMode = () => {
@@ -39,9 +42,14 @@ const BtnDarkMode = () => {
     }
 
 
+    const btnNormal = 'dark-mode-btn'
+    const btnActive = 'dark-mode-btn dark-mode-btn--active'
+
     return (
         <>
-            <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarkMode}>
+            <button
+                // ref={btnRef}
+                className={darkMode === 'dark' ? btnActive : btnNormal} onClick={toggleDarkMode}>
                 <img src={sun} alt="Light mode" className="dark-mode-btn__icon"/>
                 <img src={moon} alt="Dark mode" className="dark-mode-btn__icon"/>
             </button>
